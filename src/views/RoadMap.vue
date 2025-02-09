9bd90d410bb082feacabf1d33d22c348455d5eb7
JMusilek <jmusilek@gk-software.com>
2025-02-09 01:14:43 +0100

    Road map - behavior - click - rail - only for avatar and not buttons

--- src/views/RoadMap.vue
+++ src/views/RoadMap.vue
@@ -4,12 +4,13 @@
       <v-navigation-drawer
           v-model="drawer"
           :rail="rail"
+          @click.stop="rail = !rail"
           permanent
-          @click="rail = false"
       >
         <v-list-item
             :prepend-avatar="avatarPath"
             title="Hry pro děti"
+            @click.stop="rail = !rail"
             nav
         >
           <template v-slot:append>
@@ -30,36 +31,42 @@
               title="Home"
               value="home"
               router to="/home"
+              @click.stop="rail = rail"
           ></v-list-item>
           <v-list-item
               prepend-icon="mdi-account"
               title="About"
               value="/about"
               router to="/about"
+              @click.stop="rail = rail"
           ></v-list-item>
           <v-list-item
               prepend-icon="mdi-school"
               title="Sčítání odečítání - UČENÍ"
               value="scitani-odecitani-uceni"
               router to="/scitani-odecitani-uceni"
+              @click.stop="rail = rail"
           ></v-list-item>
           <v-list-item
               prepend-icon="mdi-numeric"
               title="Sčítání odečítání"
               value="scitani-odecitani"
               router to="/scitani-odecitani"
+              @click.stop="rail = rail"
           ></v-list-item>
           <v-list-item
               prepend-icon="mdi-apple-keyboard-control"
               title="Rozklad"
               value="rozklad"
               router to="/rozklad"
+              @click.stop="rail = rail"
           ></v-list-item>
           <v-list-item
               prepend-icon="mdi-multimedia"
               title="Animace - hřiště"
               value="animace"
               router to="/animation-playground"
+              @click.stop="rail = rail"
           ></v-list-item>
           <v-list-item
               v-if="isLoggedIn && loggedUser?.username === 'admin'"
@@ -67,6 +74,7 @@
               title="CRUD Test"
               value="crud-test"
               router to="/crud-test"
+              @click.stop="rail = rail"
           ></v-list-item>
         </v-list>
 
@@ -77,8 +85,8 @@
             <pre v-html="JSON.stringify(loggedUser, null, 2)"></pre>
           </v-card>
           <LoginUserFlow/>
+          <v-btn v-if="isLoggedIn && loggedUser?.username === 'admin'" small @click="toggleDebugMode">Toggle Debug Mode</v-btn>
         </div>
-  <v-btn v-if="isLoggedIn && loggedUser?.username === 'admin'" small @click="toggleDebugMode">Toggle Debug Mode</v-btn>
 
 
 
